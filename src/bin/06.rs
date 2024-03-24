ce87091195994d6015dbcd4fd5ade9a32f31af08
Richard Clamp <richardc@unixbeard.net>
2024-03-24 18:34:00 +0000

    Day 6 Part 2: Parameterise the run_length
    So yes, it makes sense to give that a binding, as for part 2 we need to
    check for runs of 14 distinct values rather than 4.

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -2,23 +2,22 @@ use std::collections::HashSet;
 
 advent_of_code::solution!(6);
 
-fn start_index(s: &str) -> usize {
+fn start_index(s: &str, run_length: usize) -> usize {
     s.as_bytes()
-        .windows(4)
+        .windows(run_length)
         .enumerate()
-        .find(|(_, w)| HashSet::<u8>::from_iter(w.iter().copied()).len() == 4)
+        .find(|(_, w)| HashSet::<u8>::from_iter(w.iter().copied()).len() == run_length)
         .unwrap()
         .0
-        + 4
+        + run_length
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
-    Some(start_index(input))
+    Some(start_index(input, 4))
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    _ = input;
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    Some(start_index(input, 14))
 }
 
 #[cfg(test)]
@@ -33,7 +32,7 @@ mod tests {
     #[case("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
     #[case("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
     fn start_index_test(#[case] input: &str, #[case] expected: usize) {
-        assert_eq!(expected, start_index(input))
+        assert_eq!(expected, start_index(input, 4))
     }
 
     #[test]
@@ -45,6 +44,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(19));
     }
 }
