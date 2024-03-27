8fb7f2f7bc14f9dc12c07a3fe1b10cc219bd1cbf
Richard Clamp <richardc@unixbeard.net>
2024-03-27 18:37:54 +0000

    Day 10 Part 1: Simple evaluate of the program
    Rather than do a full machine code thingy, we just expand the
    instruction stream into a vec of things to add.  Noop adds 0, Addx 49
    adds 0, 49 encoding the one tick of delay.

---
+++ src/bin/10.rs
@@ -0,0 +1,50 @@
+advent_of_code::solution!(10);
+
+use itertools::Itertools;
+
+pub fn part_one(input: &str) -> Option<i32> {
+    let program = input
+        .lines()
+        .flat_map(|l| {
+            if let Some(value) = l.strip_prefix("addx ") {
+                let value = value.parse::<i32>().expect("to be a number");
+                vec![0, value]
+            } else {
+                vec![0]
+            }
+        })
+        .collect_vec();
+
+    let mut x = 1;
+    let mut points = Vec::new();
+    for (i, v) in (1..=220).zip(program.iter().cycle()) {
+        if (i - 20) % 40 == 0 {
+            points.push(i * x);
+        }
+        x += v;
+    }
+
+    Some(points.iter().sum())
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    _ = input;
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(13140));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
