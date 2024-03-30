baa1526113799962150a3d3b5f6dd1a5723526df
Richard Clamp <richardc@unixbeard.net>
2024-03-30 11:07:16 +0000

    Day 15 Part 1: Bruteish computation of excluded_cells
    This is a little brute force.  We compute a few sets of ranges, and then
    punch them into a HashSet just so we can count the entries.  For the small
    example this is fine, but it's almost a second in runtime for the actual
    solve.
    
            Running `target/release/15`
        Part 1: 4717631 (894.3ms)
    
    More efficient would be to actually maintain a set of ranges, merging
    up, and then splitting out the actual beacons.  Will see what part 2
    needs first.

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -6,10 +6,15 @@ use std::collections::HashSet;
 #[derive(Debug)]
 struct Point(i32, i32);
 
+impl Point {
+    fn manhattan(&self, other: &Self) -> u32 {
+        self.0.abs_diff(other.0) + self.1.abs_diff(other.1)
+    }
+}
+
 #[derive(Debug)]
 struct Sensors {
     sensors: Vec<(Point, Point)>,
-    excluded: HashSet<Point>,
 }
 
 fn sensor(input: &str) -> IResult<&str, (Point, Point)> {
@@ -30,12 +35,31 @@ fn sensor(input: &str) -> IResult<&str, (Point, Point)> {
 impl Sensors {
     fn new(s: &str) -> Self {
         let sensors = s.lines().map(|l| sensor(l).unwrap().1).collect();
-        let excluded = HashSet::new();
-        Self { sensors, excluded }
+        Self { sensors }
     }
 
     fn excluded_cells(&self, y: i32) -> usize {
-        self.excluded.iter().filter(|Point(_, ey)| *ey == y).count()
+        let mut excluded: HashSet<i32> = HashSet::new();
+        for (sensor, beacon) in &self.sensors {
+            let distance = sensor.manhattan(&beacon);
+            let intersect = sensor.1 - y;
+            if intersect.unsigned_abs() > distance {
+                // No overlap
+                continue;
+            }
+
+            let x_min = sensor.0 - (distance as i32 - intersect.abs()).abs();
+            let x_max = sensor.0 + (distance as i32 - intersect.abs()).abs();
+            excluded.extend(x_min..=x_max);
+        }
+
+        for (_, beacon) in &self.sensors {
+            if beacon.1 == y {
+                excluded.remove(&beacon.0);
+            }
+        }
+
+        excluded.len()
     }
 }
 
