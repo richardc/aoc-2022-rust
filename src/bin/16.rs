d8a0bd10a67f00913772b4e74bf8cdebf159eb3a
Richard Clamp <richardc@unixbeard.net>
2024-03-31 17:07:31 +0100

    Day 16, Part 1: BFS over a reduced network
    We use the Floyd-Warshall algorithm to determine all the shortest paths
    in the graph between two nodes.
    
    Then we extract the `interesting` nodes to identify all the valves that
    will do something, and do a BFS reach from 'AA' to all 'interesting'
    nodes, using the distances array to calculate the steam flow for this
    walk.
    
    We use a bitmap from the Bitmaps crate to track which of the interesting
    nodes are still closed, because it hashes so can used for the state
    handled by bfs_reach.
    
    Bashed out head against a brick wall for quite a while before realising
    that we'd assumed valve[0] == valve.AA.  That's true for the example,
    but *not* for the real data.

---
+++ src/bin/16.rs
@@ -0,0 +1,135 @@
+use bitmaps::Bitmap;
+use ndarray::Array2;
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{alpha1, u32},
+    multi::separated_list0,
+    sequence::tuple,
+    IResult,
+};
+use pathfinding::directed::bfs::bfs_reach;
+use std::collections::HashMap;
+
+advent_of_code::solution!(16);
+
+fn valve(input: &str) -> IResult<&str, (&str, u32, Vec<&str>)> {
+    let (input, (_, id, _, flow, _, edges)) = tuple((
+        tag("Valve "),
+        alpha1,
+        tag(" has flow rate="),
+        u32,
+        alt((
+            tag("; tunnels lead to valves "),
+            tag("; tunnel leads to valve "),
+        )),
+        separated_list0(tag(", "), alpha1),
+    ))(input)?;
+
+    Ok((input, (id, flow, edges)))
+}
+
+struct Valves<'a> {
+    valves: Vec<(&'a str, u32, Vec<&'a str>)>,
+}
+
+impl<'b> Valves<'b> {
+    fn new<'a>(s: &'a str) -> Valves<'a>
+    where
+        'a: 'b,
+        'b: 'a,
+    {
+        let valves = s.lines().map(|l| valve(l).unwrap().1).collect();
+        Self { valves }
+    }
+
+    fn most_released(&self, duration: u32) -> u32 {
+        let ids: HashMap<&str, usize> = HashMap::from_iter(
+            self.valves
+                .iter()
+                .enumerate()
+                .map(|(i, &(name, _, _))| (name, i)),
+        );
+        let interesting: Vec<usize> = self
+            .valves
+            .iter()
+            .filter(|(_, flow, _)| *flow > 0)
+            .map(|(name, _, _)| *ids.get(name).unwrap())
+            .collect();
+
+        // Floyd-Warshall algorithm
+        let mut distances = Array2::from_elem((self.valves.len(), self.valves.len()), u32::MAX);
+        for i in 0..self.valves.len() {
+            distances[[i, i]] = 0;
+        }
+        for i in 0..self.valves.len() {
+            for edge in &self.valves[i].2 {
+                let j = *ids.get(edge).unwrap();
+                distances[[i, j]] = 1;
+                distances[[j, i]] = 1;
+            }
+        }
+        for k in 0..self.valves.len() {
+            for i in 0..self.valves.len() {
+                for j in 0..self.valves.len() {
+                    if let Some(distance) = distances[[i, k]].checked_add(distances[[k, j]]) {
+                        if distances[[i, j]] > distance {
+                            distances[[i, j]] = distance;
+                        }
+                    }
+                }
+            }
+        }
+
+        assert!(interesting.len() <= 16);
+        let start = *ids.get("AA").unwrap();
+        let closed: Bitmap<16> = Bitmap::mask(interesting.len());
+        bfs_reach((start, 0, closed, 0), |(node, time, closed, released)| {
+            let mut next = Vec::new();
+            for index in closed {
+                let neighbour = interesting[index];
+                let time = time + distances[[*node, neighbour]] + 1;
+                if time > duration {
+                    continue;
+                }
+                let mut closed = *closed;
+                closed.set(index, false);
+                let flow_added = self.valves[neighbour].1;
+                let remaining_time = duration - time;
+                let released = released + remaining_time * flow_added;
+                next.push((neighbour, time, closed, released))
+            }
+            next
+        })
+        .map(|(_, _, _, released)| released)
+        .max()
+        .expect("to have a max")
+    }
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let valves = Valves::new(input);
+    Some(valves.most_released(30))
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    _ = input;
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(1651));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
