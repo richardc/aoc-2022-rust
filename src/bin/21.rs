af47fca00b365937f89358515c9784e47b87e3ef
Richard Clamp <richardc@unixbeard.net>
2024-04-05 12:21:14 +0100

    Refactor tree to be a real tree.
    For part 2 we can find our input variable by identifying the parent of
    'root' with humn as an ancestor, and the one without.  The one without
    gives the contstant we want to get to, so we could walk the tree
    backwards up to humn computing the inverse of each step.
    
    We didn't build our tree really as a tree though, so some of the
    places we'd want to walk back up were getting lost in lack of helpers.
    
    This commit changes to a tree rooted at the 'root' element.  We tossed
    out nom as it wasn't helping that much as we needed two passes over the
    data, once to break up the names from thier definitions, and then a
    second to recursively turn the expressions into nested tree nodes.

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -1,131 +1,159 @@
 use std::collections::HashMap;
 
-use nom::{
-    branch::alt, bytes::complete::tag, character::complete::alpha1, character::complete::i64,
-    sequence::tuple, IResult,
-};
-
 advent_of_code::solution!(21);
 
 type Value = i64;
 type Name = [u8; 4];
 
-enum Kind {
+#[derive(Debug, Clone, Copy)]
+enum Op {
     Add,
     Sub,
     Div,
     Mul,
 }
 
-impl Kind {
+impl Op {
     fn new(s: &str) -> Self {
         match s {
-            "+" => Kind::Add,
-            "-" => Kind::Sub,
-            "*" => Kind::Mul,
-            "/" => Kind::Div,
+            "+" => Op::Add,
+            "-" => Op::Sub,
+            "*" => Op::Mul,
+            "/" => Op::Div,
             _ => unreachable!("not an operation"),
         }
     }
+
+    fn apply(&self, left: Value, right: Value) -> Value {
+        match self {
+            Op::Add => left + right,
+            Op::Sub => left - right,
+            Op::Mul => left * right,
+            Op::Div => left / right,
+        }
+    }
 }
 
+#[derive(Debug)]
 struct Operation {
-    kind: Kind,
-    left: Name,
-    right: Name,
+    op: Op,
+    left: Monkey,
+    right: Monkey,
 }
 
+#[derive(Debug)]
 enum Monkey {
+    Variable(Value),
     Value(Value),
-    Op(Operation),
+    Op(Box<Operation>),
 }
 
-struct Monkeys {
-    all: HashMap<Name, Monkey>,
+fn parse_monkeys(input: &str) -> Monkey {
+    let named: HashMap<&str, &str> =
+        HashMap::from_iter(input.lines().map(|l| l.split_once(": ").unwrap()));
+
+    Monkey::from_str("root", &named)
 }
 
-fn monkey_value(input: &str) -> IResult<&str, Monkey> {
-    let (input, value) = i64(input)?;
-
-    Ok((input, Monkey::Value(value)))
-}
-
-fn monkey_op(input: &str) -> IResult<&str, Monkey> {
-    let (input, (left, _, op, _, right)) = tuple((
-        alpha1,
-        tag(" "),
-        alt((tag("+"), tag("-"), tag("/"), tag("*"))),
-        tag(" "),
-        alpha1,
-    ))(input)?;
-
-    Ok((
-        input,
-        Monkey::Op(Operation {
-            left: (&left.as_bytes()[..4]).try_into().expect("bytes"),
-            right: (&right.as_bytes()[..4]).try_into().expect("bytes"),
-            kind: Kind::new(op),
-        }),
-    ))
-}
-
-fn monkey_line(input: &str) -> IResult<&str, (Name, Monkey)> {
-    let (input, (name, _, monkey)) =
-        tuple((alpha1, tag(": "), alt((monkey_value, monkey_op))))(input)?;
-
-    Ok((
-        input,
-        ((&name.as_bytes()[..4]).try_into().expect("bytes"), monkey),
-    ))
-}
-
-fn parse_monkey(s: &str) -> (Name, Monkey) {
-    monkey_line(s).expect("parse").1
-}
-
-impl Monkeys {
-    fn new(input: &str) -> Self {
-        let all = HashMap::from_iter(input.lines().map(parse_monkey));
-        Self { all }
+impl Monkey {
+    fn from_str(name: &str, dict: &HashMap<&str, &str>) -> Self {
+        let def = dict.get(name).unwrap();
+        if let Ok(value) = def.parse::<Value>() {
+            if name == "humn" {
+                Monkey::Variable(value)
+            } else {
+                Monkey::Value(value)
+            }
+        } else {
+            let args: Vec<_> = def.split(' ').collect();
+            let left = Monkey::from_str(args[0], dict);
+            let op = Op::new(args[1]);
+            let right = Monkey::from_str(args[2], dict);
+            Monkey::Op(Box::new(Operation { left, right, op }))
+        }
     }
 
-    fn cached_eval(&self, name: &Name, cache: &mut HashMap<Name, Value>) -> Value {
-        if let Some(value) = cache.get(name) {
-            return *value;
-        }
-
-        let monkey = self.all.get(name).expect("known monkey");
-        let value = match &monkey {
-            Monkey::Value(v) => *v,
+    fn eval(&self) -> Value {
+        match &self {
+            Monkey::Value(v) | Monkey::Variable(v) => *v,
             Monkey::Op(op) => {
-                let left = self.cached_eval(&op.left, cache);
-                let right = self.cached_eval(&op.right, cache);
-                match op.kind {
-                    Kind::Add => left + right,
-                    Kind::Sub => left - right,
-                    Kind::Mul => left * right,
-                    Kind::Div => left / right,
-                }
+                let left = op.left.eval();
+                let right = op.right.eval();
+                op.op.apply(left, right)
             }
+        }
+    }
+
+    fn fold_tree(&mut self) {
+        if let Monkey::Op(node) = self {
+            let Operation { left, right, op } = node.as_mut();
+            left.fold_tree();
+            right.fold_tree();
+            if let (Monkey::Value(left), Monkey::Value(right)) = (left, right) {
+                *self = Monkey::Value(op.apply(*left, *right));
+            }
+        }
+    }
+
+    fn have_humn(&self) -> bool {
+        if let Monkey::Variable(_) = self {
+            return true;
+        }
+        if let Monkey::Op(node) = self {
+            if node.left.have_humn() {
+                return true;
+            }
+            if node.right.have_humn() {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn into_children(self) -> (Monkey, Op, Monkey) {
+        if let Monkey::Op(node) = self {
+            let Operation { left, op, right } = *node;
+            (left, op, right)
+        } else {
+            unreachable!("should only be called on an op")
+        }
+    }
+
+    fn uneval(self, accum: &mut Value) -> Self {
+        let (left, op, right) = self.into_children();
+
+        left
+    }
+
+    fn human_say(mut self) -> Value {
+        self.fold_tree();
+        let Monkey::Op(node) = self else {
+            unreachable!("should only be called on an op");
         };
 
-        cache.insert(*name, value);
-        value
-    }
+        let Operation { left, right, .. } = *node;
+        let (constant, mut humn) = if left.have_humn() {
+            (right, left)
+        } else {
+            (left, right)
+        };
 
-    fn eval(&self, name: &Name) -> Value {
-        self.cached_eval(name, &mut HashMap::new())
+        let mut target = constant.eval();
+        while !matches!(humn, Monkey::Variable(_)) {
+            humn = humn.uneval(&mut target);
+        }
+        target
     }
 }
 
 pub fn part_one(input: &str) -> Option<Value> {
-    let monkeys = Monkeys::new(input);
-    Some(monkeys.eval(b"root"))
+    let root = parse_monkeys(input);
+    Some(root.eval())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    _ = input;
-    None
+pub fn part_two(input: &str) -> Option<Value> {
+    let root = parse_monkeys(input);
+    Some(root.human_say())
 }
 
 #[cfg(test)]
@@ -141,6 +169,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(301));
     }
 }
