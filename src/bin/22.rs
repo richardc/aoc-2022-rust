114877cb4626f2697f9f249c575deb8029176758
Richard Clamp <richardc@unixbeard.net>
2024-04-07 11:55:13 +0100

    Day 22 Part 2: Implement the CubeNet zooper
    Needed paper, a rubik's cube, a sharpie, and lots of talking to myself
    to encode both the nets, and the CubeNet Zooper implementation.
    
    Made some use of tarpaulin to get a coverage report to flush out the
    bugs in the CubeNet Zooper trait, as there are a lot of caces to verify.
    
    Tarpaulin default to deleting your target dir because it has trust issues
    around recompilation not tracking RUSTFLAGS correctly.   To avoid constant
    rebuilds, give it its own target-dir eg:
    
        cargo tarpaulin --target-dir tarp-target --bin 22 --out html

--- src/bin/22.rs
+++ src/bin/22.rs
@@ -6,7 +6,7 @@ use winnow::prelude::*;
 
 advent_of_code::solution!(22);
 
-#[derive(Debug, Default, Clone, Copy)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
 enum Facing {
     #[default]
     Right = 0,
@@ -198,8 +198,8 @@ struct Pacman {
 }
 
 impl Pacman {
-    fn new(map: Map) -> Self {
-        Self { map }
+    fn new(map: &Map) -> Self {
+        Self { map: map.clone() }
     }
 }
 
@@ -216,24 +216,200 @@ impl Zooper for Pacman {
     }
 }
 
-struct CubeNet {}
+#[derive(Debug, Default)]
+struct Face {
+    position: (usize, usize),
+    connections: HashMap<Facing, (usize, Facing)>,
+}
+
+struct CubeNet {
+    width: usize,
+    faces: [Face; 6],
+}
+
+// There are 11 Nets of a cube, we only have encoded 2.   A future enhancement might be to
+// generate the other 9 and search for a match.  That or we could write a routine that would
+// generate a net from a raw map, as that wouldn't need to account for the rotations.
+impl CubeNet {
+    fn new(map: &Map) -> Self {
+        if map.tiles.len() == 4 * 4 * 6 {
+            Self::example_net()
+        } else {
+            Self::puzzle_net()
+        }
+    }
+
+    fn puzzle_net() -> Self {
+        use Facing::*;
+        // The net from my puzzle input
+        //  | 0 1 2
+        //--+------
+        // 0|   0 1
+        // 1|   2
+        // 2| 3 4
+        // 3| 5
+        Self {
+            width: 50,
+            faces: [
+                // 0
+                Face {
+                    position: (0, 1),
+                    connections: HashMap::from_iter([(Up, (5, Right)), (Left, (3, Right))]),
+                },
+                // 1
+                Face {
+                    position: (0, 2),
+                    connections: HashMap::from_iter([
+                        (Down, (2, Left)),
+                        (Up, (5, Up)),
+                        (Right, (4, Left)),
+                    ]),
+                },
+                // 2
+                Face {
+                    position: (1, 1),
+                    connections: HashMap::from_iter([(Left, (3, Down)), (Right, (1, Up))]),
+                },
+                // 3
+                Face {
+                    position: (2, 0),
+                    connections: HashMap::from_iter([(Up, (2, Right)), (Left, (0, Right))]),
+                },
+                // 4
+                Face {
+                    position: (2, 1),
+                    connections: HashMap::from_iter([(Right, (1, Left)), (Down, (5, Left))]),
+                },
+                // 5
+                Face {
+                    position: (3, 0),
+                    connections: HashMap::from_iter([
+                        (Left, (0, Down)),
+                        (Right, (4, Up)),
+                        (Down, (1, Down)),
+                    ]),
+                },
+            ],
+        }
+    }
+
+    fn example_net() -> Self {
+        use Facing::*;
+        // The net of the example
+        //    0 1 2 3
+        // 0      0
+        // 1  1 2 3
+        // 2      4 5
+        Self {
+            width: 4,
+            faces: [
+                // 0
+                Face {
+                    position: (0, 2),
+                    connections: HashMap::from_iter([
+                        (Left, (2, Down)),
+                        (Right, (5, Left)),
+                        (Up, (1, Down)),
+                    ]),
+                },
+                // 1
+                Face {
+                    position: (1, 0),
+                    connections: HashMap::from_iter([
+                        (Left, (5, Up)),
+                        (Up, (0, Down)),
+                        (Down, (4, Up)),
+                    ]),
+                },
+                // 2
+                Face {
+                    position: (1, 1),
+                    connections: HashMap::from_iter([(Up, (0, Right)), (Down, (4, Right))]),
+                },
+                // 3
+                Face {
+                    position: (1, 2),
+                    connections: HashMap::from_iter([(Right, (5, Down))]),
+                },
+                // 4
+                Face {
+                    position: (2, 2),
+                    connections: HashMap::from_iter([(Left, (2, Up)), (Down, (1, Up))]),
+                },
+                // 5
+                Face {
+                    position: (2, 3),
+                    connections: HashMap::from_iter([
+                        (Right, (0, Left)),
+                        (Up, (3, Left)),
+                        (Down, (1, Right)),
+                    ]),
+                },
+            ],
+        }
+    }
+}
 
 impl Zooper for CubeNet {
     fn step(&self, point: (usize, usize), direction: Facing) -> ((usize, usize), Facing) {
-        (point, direction)
+        // Find the face we're starting on
+        let face_position = ((point.0 - 1) / self.width, (point.1 - 1) / self.width);
+
+        let face = self
+            .faces
+            .iter()
+            .find(|face| face.position == face_position)
+            .expect("should be a face in the net");
+        let (next_face, next_direction) = face
+            .connections
+            .get(&direction)
+            .expect("should have a connection on an edge");
+
+        let next_face = &self.faces[*next_face];
+
+        // Map from 1-based global coordinates to 0-based face coordinates
+        let (face_row, face_column) = ((point.0 - 1) % self.width, (point.1 - 1) % self.width);
+        let last = self.width - 1;
+
+        use Facing::*;
+        let (next_face_row, next_face_column) = match (direction, next_direction) {
+            (Right, Right) => (face_row, 0),
+            (Left, Left) => (face_row, last),
+            (Up, Up) => (last, face_column),
+            (Down, Down) => (0, face_column),
+            (Right, Left) => (last - face_row, last),
+            (Left, Right) => (last - face_row, 0),
+            (Up, Down) => (0, last - face_column),
+            (Down, Up) => (last, last - face_column),
+            (Right, Down) => (0, last - face_row),
+            (Down, Left) => (face_column, last),
+            (Left, Up) => (last, last - face_row),
+            (Up, Right) => (face_column, 0),
+            (Right, Up) => (last, face_row),
+            (Down, Right) => (last - face_column, 0),
+            (Left, Down) => (0, face_row),
+            (Up, Left) => (last - face_column, last),
+        };
+
+        // Map back to the global 1-indexed position
+        let next_point = (
+            next_face_row + next_face.position.0 * self.width + 1,
+            next_face_column + next_face.position.1 * self.width + 1,
+        );
+        (next_point, *next_direction)
     }
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
     let map = Map::new(input);
-    let mut walker = Walker::new(&map, Box::new(Pacman::new(map.clone())));
+    let mut walker = Walker::new(&map, Box::new(Pacman::new(&map)));
     walker.walk(&map);
     Some(walker.sum())
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
     let map = Map::new(input);
-    let mut walker = Walker::new(&map, Box::new(CubeNet {}));
+    let mut walker = Walker::new(&map, Box::new(CubeNet::new(&map)));
     walker.walk(&map);
     Some(walker.sum())
 }
@@ -252,5 +428,47 @@ mod tests {
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(5031));
+    }
+
+    mod cubenet {
+        use super::*;
+        use rstest::rstest;
+        use Facing::*;
+
+        #[rstest]
+        #[case::example_a_right_b(((6,12), Right), ((9,15), Down))]
+        #[case::example_b_up_a(((9,15), Up), ((6,12), Left))]
+        #[case::example_c_down_d(((12,11), Down), ((8,2), Up))]
+        #[case::example_d_up_c(((8,2), Down), ((12,11), Up))]
+        #[case::north_wrap(((1,9), Up), ((5,4), Down))]
+        #[case::north_wrap_inverse(((5,4), Up), ((1,9), Down))]
+        #[case::right_left(((2,12), Right), ((11,16), Left))]
+        #[case::right_left_inverse(((11,16), Right), ((2,12), Left))]
+        #[case::left_down(((1,9), Left), ((5,5), Down))]
+        #[case::left_down_inverse(((5,5), Up), ((1,9), Right))]
+        #[case::down_right(((12,16), Down), ((5,1), Right))]
+        #[case::down_right_inverse(((5,1), Left), ((12,16), Up))]
+        fn test_example_zoop(
+            #[case] start: ((usize, usize), Facing),
+            #[case] expected: ((usize, usize), Facing),
+        ) {
+            let zooper = CubeNet::example_net();
+            let result = zooper.step(start.0, start.1);
+            assert_eq!(result, expected);
+        }
+
+        #[rstest]
+        #[case::down_left(((50,150), Down), ((100,100), Left))]
+        #[case::down_left_inverse(((100,100), Right), ((50,150), Up))]
+        #[case::left_right(((1,51), Left), ((150,1), Right))]
+        #[case::left_right_inverse(((150,1), Left), ((1,51), Right))]
+        fn test_puzzle_zoop(
+            #[case] start: ((usize, usize), Facing),
+            #[case] expected: ((usize, usize), Facing),
+        ) {
+            let zooper = CubeNet::puzzle_net();
+            let result = zooper.step(start.0, start.1);
+            assert_eq!(result, expected);
+        }
     }
 }
